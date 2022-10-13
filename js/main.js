13146db37a930860b15afeebe04e787b071de3fc
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-10-14 06:04:58 +0800

    final web dev project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.querySelector('.fave').style.color = "#E9EFE6";
+
+function radioButton() {
+    alert('Tell us why this is your fave!')
+}
+radioButton();
+
+let myVariable = "Jump in!";
+    console.log(myVariable);
+    
+document.getElementById("venom").style.backgroundColor = "#904C77";
+
+function mouseOver() {
+document.getElementById("Contact1").style.color = "#904C77";
+let MouseOver = "Get in contact.";
+ console.log(MouseOver);
+    }
+
+
+
+
