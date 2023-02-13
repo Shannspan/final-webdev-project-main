a6e8f3e76a103635e32f266131c00e4138f23869
[Shann Roberts] <[dpr.1979@bigpond.com]>
2023-02-13 12:22:14 +0800

    ideas for fix on error 'required' causes

--- js/contact.js
+++ js/contact.js
@@ -4,27 +4,37 @@ function mouseOver() {
      console.log(MouseOver);
         }
 
-document.getElementById("newsletter").addEventListener("change", function() {
-    if (this.value === "email") {
-              myEmail();
-    } else if (this.value === "post") {
-              myPost();
-    }
-    });
+//BELOW CODE WORKS BUT PRODUCES ERROR
+// FIX IS NEEDED IN REGARD TO 
+// INPUT EMAIL & POST 'REQUIRED' 
+// SO WILL WORK ONCE BOTH DETAILS ARE ENTERED BUT
+// INDIVIDUALLY PRODUCES ERROR.
+// THIS CODE 
+//document.getElementById('element_id').removeAttribute('required');
+//DID NOT WORK IN EACH INDIVIDUAL FUNCTION
+// CONSIDER OTHER APPLICATIONS
 
-function myEmail() {
-    let emailInput = document.getElementById("email");
-    let postInput = document.getElementById("post");
-    emailInput.style.display ="block";
-    postInput.style.display = "none";
-    };
+// document.getElementById("newsletter").addEventListener("change", function() {
+//     if (this.value === "email") {
+//               myEmail();
+//     } else if (this.value === "post") {
+//               myPost();
+//     }
+//     });
 
-function myPost() {
-    let emailInput = document.getElementById("email");
-    let postInput = document.getElementById("post");
-    emailInput.style.display ="none";
-    postInput.style.display = "block";
-    };
+// function myEmail() {
+//     let emailInput = document.getElementById("email");
+//     let postInput = document.getElementById("post");
+//     emailInput.style.display ="block";
+//     postInput.style.display = "none";
+//     };
+
+// function myPost() {
+//     let emailInput = document.getElementById("email");
+//     let postInput = document.getElementById("post");
+//     emailInput.style.display ="none";
+//     postInput.style.display = "block";
+//     };
 
 
 
